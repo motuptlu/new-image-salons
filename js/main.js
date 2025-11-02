77bfdb01c00f380fbef79cbd958815196d70a166
motuptlu <motupatlu8875@gmail.com>
2025-11-03 00:38:55 +0530

    Implement GSAP animations and ScrollTrigger
    Added GSAP animations for various elements and scenes.

--- js/main.js
+++ js/main.js
@@ -639,4 +639,107 @@ window.closeLightbox = closeLightbox;
 window.changeLightboxImage = changeLightboxImage;
 window.shareOnFacebook = shareOnFacebook;
 window.shareOnTwitter = shareOnTwitter;
-window.shareOnWhatsApp = shareOnWhatsApp;
+window.shareOnWhatsApp = shareOnWhatsApp; gsap.registerPlugin(ScrollTrigger);
+
+let speed = 100;
+let height = document.querySelector("svg").getBBox().height;
+
+// Initial setup
+gsap.set("#h2-1", { opacity: 0 });
+gsap.set("#bg_grad", { attr: { cy: "-50" } });
+gsap.set(["#dinoL", "#dinoR"], { y: 80 });
+gsap.set("#dinoL", { x: -10 });
+
+// Responsive setup
+const mm = gsap.matchMedia();
+mm.add("(max-width: 1922px)", () => {
+  gsap.set(["#cloudStart-L", "#cloudStart-R"], { x: 10, opacity: 1 });
+});
+
+/* SCENE 1 */
+let scene1 = gsap.timeline();
+ScrollTrigger.create({
+  animation: scene1,
+  trigger: ".scrollElement",
+  start: "top top",
+  end: "45% 100%",
+  scrub: 3
+});
+
+// Hills animation
+scene1.to("#h1-1", { y: 3 * speed, x: 1 * speed, scale: 0.9, ease: "power1.in" }, 0);
+scene1.to("#h1-2", { y: 2.6 * speed, x: -0.6 * speed, ease: "power1.in" }, 0);
+scene1.to("#h1-3", { y: 1.7 * speed, x: 1.2 * speed }, 0.03);
+scene1.to("#h1-4", { y: 3 * speed, x: 1 * speed }, 0.03);
+scene1.to("#h1-5", { y: 2 * speed, x: 1 * speed }, 0.03);
+scene1.to("#h1-6", { y: 2.3 * speed, x: -2.5 * speed }, 0);
+scene1.to("#h1-7", { y: 5 * speed, x: 1.6 * speed }, 0);
+scene1.to("#h1-8", { y: 3.5 * speed, x: 0.2 * speed }, 0);
+scene1.to("#h1-9", { y: 3.5 * speed, x: -0.2 * speed }, 0);
+scene1.to("#cloudsBig-L", { y: 4.5 * speed, x: -0.2 * speed }, 0);
+scene1.to("#cloudsBig-R", { y: 4.5 * speed, x: -0.2 * speed }, 0);
+scene1.to("#cloudStart-L", { x: -300 }, 0);
+scene1.to("#cloudStart-R", { x: 300 }, 0);
+
+// Animate text
+scene1.to("#info", { y: 8 * speed }, 0);
+
+/* Bird */
+gsap.fromTo("#bird", { opacity: 1 }, {
+  y: -250,
+  x: 800,
+  ease: "power2.out",
+  scrollTrigger: {
+    trigger: ".scrollElement",
+    start: "15% top",
+    end: "60% 100%",
+    scrub: 4,
+    onEnter: () => gsap.to("#bird", { scaleX: 1, rotation: 0 }),
+    onLeave: () => gsap.to("#bird", { scaleX: -1, rotation: -15 })
+  }
+});
+
+/* Clouds */
+let clouds = gsap.timeline();
+ScrollTrigger.create({
+  animation: clouds,
+  trigger: ".scrollElement",
+  start: "top top",
+  end: "70% 100%",
+  scrub: 1
+});
+
+clouds.to("#cloud1", { x: 500 }, 0);
+clouds.to("#cloud2", { x: 1000 }, 0);
+clouds.to("#cloud3", { x: -1000 }, 0);
+clouds.to("#cloud4", { x: -700, y: 25 }, 0);
+
+/* Sun Motion Animation */
+let sun = gsap.timeline();
+ScrollTrigger.create({
+  animation: sun,
+  trigger: ".scrollElement",
+  start: "1% top",
+  end: "2150 100%",
+  scrub: 2
+});
+
+sun.fromTo("#bg_grad", { attr: { cy: "-50" } }, { attr: { cy: "330" } }, 0);
+sun.to("#bg_grad stop:nth-child(6)", { attr: { "stop-color": "#FF9171" } }, 0);
+
+/* SCENE 2 */
+let scene2 = gsap.timeline();
+ScrollTrigger.create({
+  animation: scene2,
+  trigger: ".scrollElement",
+  start: "15% top",
+  end: "40% 100%",
+  scrub: 3
+});
+
+scene2.fromTo("#h2-1", { y: 500, opacity: 0 }, { y: 0, opacity: 1 }, 0);
+scene2.fromTo("#h2-2", { y: 500 }, { y: 0 }, 0.1);
+scene2.fromTo("#h2-3", { y: 700 }, { y: 0 }, 0.1);
+scene2.fromTo("#h2-4", { y: 700 }, { y: 0 }, 0.2);
+scene2.fromTo("#h2-5", { y: 800 }, { y: 0 }, 0.3);
+scene2.fromTo("#h2-6", { y: 900 }, { y: 0 }, 0.3);
